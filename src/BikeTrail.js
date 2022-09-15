014068a34b286979f748772ca1d63677a7513869
Jordan Jennings <jordanjennings12@gmail.com>
2022-09-15 13:46:10 -0400

    details page

--- src/BikeTrail.js
+++ src/BikeTrail.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar"
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Button from 'react-bootstrap/Button';
 import { useHistory, useParams } from 'react-router-dom';
 
-function BikeTrail({ trail, trails, handleDelete, handleComments }) {
+function BikeTrail({ trails, handleDelete, handleComments }) {
   const history = useHistory();
+  const [ trail, setTrail ] = useState({})
   const [comments, setComments] = useState("")
+  const [ addComment, setAddComments ] = useState(false)
   const { id } = useParams()
+  const [ comment, setComment ] = useState("")
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/bikeTrails/${id}`)
+    .then(res => res.json())
+    .then(bikeTrails => {
+    setTrail(bikeTrails)
+    setComments(bikeTrails.comments)
+    })
+  }, [])
+
+
 
   function deleteTrail() {
     fetch(`http://localhost:3000/bikeTrails/${id}`, {
@@ -26,9 +40,9 @@ function BikeTrail({ trail, trails, handleDelete, handleComments }) {
 
   function handleSubmit(e) {
     e.preventDefault()
-    const newComments = [...trail.comments, comments]
+    const newComments = [...trail.comments, comment]
     const updatedTrail = {...trail, comments: newComments}
-    fetch(`http://localhost:3000/bikeTrails/${trail.id}`, {
+    fetch(`http://localhost:3000/bikeTrails/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -43,15 +57,17 @@ function BikeTrail({ trail, trails, handleDelete, handleComments }) {
         else{return item}
       })
       handleComments(newTrails, updatedTrail)
-      console.log(newTrails)
+      setAddComments(false)
+      setComments([...comments, comment])
     })
-    setComments("")
+    setComment("")
   }
 
   function handleChange(e) {
-    setComments(e.target.value)
+    setComment(e.target.value)
   }
 
+  const handleAddComment = () => setAddComments((addComment) => !addComment)
   
 
   return (
@@ -67,29 +83,37 @@ function BikeTrail({ trail, trails, handleDelete, handleComments }) {
       <header style={{backgroundImage:`url(${trail.image})`, height: "500px"}}>
       </header>
       <div className='specs' style={{backgroundColor: "darkgreen", fontSize: "16px", height: "60px", fontWeight: 300}}>
-        
         <p style={{color: "white"}}>Difficulty : {trail.difficulty}</p>
         <p style={{color: "white"}}>Length : {trail.lengthMiles} miles</p>
         <p style={{color: "white"}}>{trail.isHilly ? "Turraine : Hilly" : "Turraine : Flat"}</p>
-        </div>
+      </div>
       <h1 style={{marginTop: "20px"}}className='details-title'>{trail.name}</h1>
       <p style={{textAlign: "center", fontSize: "20px", color: "gray"}}>{trail.state}</p>
-      <div className="BikeTrail">
-   
-    
-        <h2>Description:</h2>
+      <div className="trail-info" style={{margin:"0 60px"}}>
+        <h3>Description</h3>
         <p>{trail.description}</p>
-        <h2>Features:</h2>
+        <h3>Features</h3>
         <p>{trail.features}</p>
-        <iframe src={trail.mapPDF}/>
-        <h2>Comments:</h2>
-        {trail.comments.map((comment, index) => <p key={index}>{comment}</p>)}
-        <form onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={comments} type="text" name="comments" placeholder='Add Comments Here!'/>
-          <input type="submit" />
-        </form>
-        <button onClick={deleteTrail}>Delete Trail</button>
+        <iframe src={trail.mapPDF} style={{height:"250px", border: "2px black solid", width: "250px"}}/>
+      <div className='comment-container'>
+        <h2 style={{paddingBottom: "20px"}}>Comments</h2>
+        <div className='add-comment'>
+          {addComment ? <form onSubmit={handleSubmit}>
+            <input onChange={handleChange} value={comment} type="text" name="comments" placeholder='Add Comments Here!' className='addComment'/>
+            <input type="submit" />
+          </form> : <Button variant="secondary" onClick={handleAddComment}>Add Comment</Button> }
+        </div>
+    
+        <div className='comments' style={{paddingTop:"30px"}}>
+          {comments.length > 0 ? comments.map((comment, index) =><div><p key={index}>{comment}</p><hr></hr></div>): null}
+        </div>
       </div>
+
+      <div style={{marginTop: "50px"}} >
+        <Button variant="outline-dark" onClick={deleteTrail}>Delete Trail</Button>
+      </div>
+        
+    </div>
     </div>
   );
 }

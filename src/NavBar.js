fa0edec9b270cf13723a7a3ec838521439b1e4e0
Joanna Bell <jjbdigital@gmail.com>
2022-09-15 07:36:51 -0500

    tweaked navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from "react-bootstrap/Button"
@@ -20,25 +19,21 @@ function NavBar({validateUser, currentUser}) {
   const handleCloseSignup = () => setShowSignup(false);
   const handleShowSignup = () => setShowSignup(true);
   
+  //<img style={{ width: "15%", height: "15%" }} src="/all-bike-trails-logo.png" alt="logo" />
 
   return (
     <>
     <div className="navbar navbar-dark bg-dark">
       <Navbar >
-        <Container>
-          <Nav className=''>
-            <img style={{ width: "20%", height: "20%" }} src="/all-bike-trails-logo.png" alt="logo" />
-            <Nav.Link href="/" >HOME</Nav.Link>
-            <Nav.Link href="/new-trail">ADD NEW TRAIL</Nav.Link>
-          </Nav>
-            
-          <Nav>
-            <Button className="px-4 mx-2 btn-secondary btn-sm rounded-pill"onClick={handleShowSignup}>Sign Up</Button>
-            <Button className="px-4 btn-secondary btn-sm rounded-pill"onClick={handleShowLogin}>Login</Button>
-          </Nav>
-        
-        </Container>
-      </Navbar>
+          <Nav >
+            <img style={{ width: "15%", height: "15%" }} src="/all-bike-trails-logo.png" alt="logo" />
+            <Button className="px-4 mx-2 btn-secondary btn-sm rounded-pill" href="/" >Home</Button>
+            <Button className="px-4 btn-secondary btn-sm rounded-pill" href="/new-trail">Add New Trail</Button>
+           </Nav>
+
+          <Button className="px-4 mx-2 btn-secondary btn-sm rounded-pill" onClick={handleShowSignup}>Sign Up</     Button>
+          <Button className="px-4 btn-secondary btn-sm rounded-pill" onClick={handleShowLogin}>Login</Button>
+        </Navbar>
     </div>
     <div className='login-modal'>
       {showLogin ? <Login showLogin={showLogin} handleCloseLogin={handleCloseLogin} validateUser={validateUser}/> : ""}
